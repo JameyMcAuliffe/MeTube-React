6b9c00d4cf650f2fe40343c9fcc9b2a636a1b734
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-08-15 12:52:59 -0500

    video renders, can also select from video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,7 +6,10 @@ const VideoList = (props) => {
 
 	const videoItems = props.videos.map((video) => {
 		//need to add a key when rendering list of items
-		return <VideoListItem key={video.etag} video={video}/>
+		return <VideoListItem 
+							onVideoSelect={props.onVideoSelect}
+							key={video.etag} 
+							video={video}/>
 	});
 
 	return (
