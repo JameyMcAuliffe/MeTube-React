8475a57c505cc22bee2581a0fc564317162aa5d6
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-08-15 13:30:43 -0500

    debounced search term function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,12 +14,17 @@ class SearchBar extends Component {
 	render() {
 		//must return some jsx
 		return (
-			<div>
+			<div className="search-bar">
 				<input
 					value={this.state.term} 
-					onChange={event => this.setState({term: event.target.value})} />
+					onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
